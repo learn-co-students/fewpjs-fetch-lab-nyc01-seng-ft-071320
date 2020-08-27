cf4f66ae8dfcb8883257fd9b8a0a1f24419c509c
DBenson1017 <devinbenson@gmail.com>
2020-08-26 20:44:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
 
-}
+  .then(function(response) {
+    return response.json();
+  })
+  
+    .then(function(json){
+      console.log(json)
+      renderBooks(json)
+    });
+  }
 
 function renderBooks(books) {
   const main = document.querySelector('main')
